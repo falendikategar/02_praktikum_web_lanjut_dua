dee14d713724c737bec361befb0b61f80dc5614f
Falendika Tegar Pratama <falendikategar@gmail.com>
2023-03-01 10:40:47 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
